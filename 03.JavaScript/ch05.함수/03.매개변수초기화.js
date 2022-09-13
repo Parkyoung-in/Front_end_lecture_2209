8dd69a36439c2cccabde24f3388845a23488584b
YoungIn Park <in951215p@gmail.com>
2022-09-13 14:15:08 +0900

    second

--- 03.JavaScript/ch05.함수/03.매개변수초기화.js
+++ 03.JavaScript/ch05.함수/03.매개변수초기화.js
@@ -9,3 +9,14 @@ function print(name, count) {
 
 print('사과', 10);
 print('감');
+
+function print2(name, count=0) {
+    // if (!count)
+    // count = 0;
+    //count = count ? count : 0;
+    // count = count || 0;
+    console.log(`${name}이/가 ${count}개 있습니다.`);
+}
+
+print2('사과', 10);
+print2('감');
